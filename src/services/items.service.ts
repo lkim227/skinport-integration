d2d0a9843ee1336a3248d9d0ffb733ad0965c76a
lkim227 <147713407+lkim227@users.noreply.github.com>
2024-11-02 07:39:33 +0100

    sorting category's logic testing

--- src/services/items.service.ts
+++ src/services/items.service.ts
@@ -13,10 +13,7 @@ if (!SKINPORT_API_URL || !SKINPORT_CLIENT_ID || !SKINPORT_CLIENT_SECRET) {
 const encodedAuth = Buffer.from(`${SKINPORT_CLIENT_ID}:${SKINPORT_CLIENT_SECRET}`).toString('base64');
 const AUTH_HEADER = `Basic ${encodedAuth}`;
 
-export const fetchItemsFromSkinport = async (): Promise<{ 
-    tradable: Item | null; 
-    nonTradable: Item | null 
-}> => {
+export const fetchItemsFromSkinport = async () => {
     const cachedItems = await redisClient.get('min_price_items');
 
     if (cachedItems) {
@@ -35,39 +32,39 @@ export const fetchItemsFromSkinport = async (): Promise<{
             }
         }
     );
-    if (!Array.isArray(response.data)) {
-        throw new Error('Invalid data format from Skinport API');
-    }
+    // if (!Array.isArray(response.data)) {
+    //     throw new Error('Invalid data format from Skinport API');
+    // }
 
-    /**
-     * Find the item with the lowest price in the specified category
-     * @param items - list of items
-     * @param isTradable - condition to check tradability
-     */
-    const findMinPriceItem = (items: Item[], isTradable: boolean): Item | null => {
-        const filteredItems = items.filter(item => 
-            (isTradable ? item.quantity > 0 : item.quantity === 0)
-        );
+    // /**
+    //  * Find the item with the lowest price in the specified category
+    //  * @param items - list of items
+    //  * @param isTradable - condition to check tradability
+    //  */
+    // const findMinPriceItem = (items: Item[], isTradable: boolean): Item | null => {
+    //     const filteredItems = items.filter(item => 
+    //         (isTradable ? item.quantity > 0 : item.quantity === 0)
+    //     );
 
-        if (filteredItems.length === 0) {
-            return null;
-        }
+    //     if (filteredItems.length === 0) {
+    //         return null;
+    //     }
 
-        return filteredItems.reduce((minItem, item) => 
-            minItem.min_price !== null && 
-            item.min_price !== null && 
-            minItem.min_price < item.min_price ? minItem : item
-        );
-    };
+    //     return filteredItems.reduce((minItem, item) => 
+    //         minItem.min_price !== null && 
+    //         item.min_price !== null && 
+    //         minItem.min_price < item.min_price ? minItem : item
+    //     );
+    // };
 
-    // Retrieve the items with minimum prices for both tradable and non-tradable items
-    const tradableItem = findMinPriceItem(response.data, true);
-    const nonTradableItem = findMinPriceItem(response.data, false);
+    // // Retrieve the items with minimum prices for both tradable and non-tradable items
+    // const tradableItem = findMinPriceItem(response.data, true);
+    // const nonTradableItem = findMinPriceItem(response.data, false);
 
-    const result = { tradable: tradableItem, nonTradable: nonTradableItem };
+    // const result = { tradable: tradableItem, nonTradable: nonTradableItem };
 
-    // Cache the result for 5 minutes
-    await redisClient.set('min_price_items', JSON.stringify(result), { EX: 300 });
+    // // Cache the result for 5 minutes
+    // await redisClient.set('min_price_items', JSON.stringify(result), { EX: 300 });
 
-    return result;
+    return response;
 };
